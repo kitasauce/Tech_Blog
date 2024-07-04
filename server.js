d003b846b9234bdb60e8168ef13f233ae7d5c386
andrew <cole.zubac@gmail.com>
2024-07-04 12:22:47 -0400

    boiler plate code for server set up

---
+++ server.js
@@ -0,0 +1,41 @@
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  cookie: {},
+  maxAge: 300000,
+  resave: false,
+  sameSite:"strict",
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.get("/", (req,res) => {
+  const logged_in = true
+ res.render("homepage", {logged_in})
+});
+
+app.get("/signup", (req,res) => {
+ const logged_in = true
+ res.render("signup", {logged_in})
+});
+
+app.get("/login", (req,res) => {
+ const logged_in = true
+ res.render("login", {logged_in})
+});
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
